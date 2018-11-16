eb605c99e283e5a032f10261f9c66cab0f7ca5d9
Alex Larson <alarson8@asu.edu>
2018-11-16 10:23:20 -0800

    phone push

--- js/addData.js
+++ js/addData.js
@@ -3,9 +3,11 @@ evt.preventDefault();
 db.collection('userInput').add({
   name: form.name.value,
   email: form.email.value,
+  phone: form.phone.value,
   message: form.message.value
   });
 form.name.value = '';
+form.phone.value= '';
 form.email.value = '';
 form.message.value = '';
 })
